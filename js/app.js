3cf738b15fcd860ccc178a3ae5cfaabf2d0015a8
Sebastiao de Paula <sebastiao.paula@gmail.com>
2025-01-20 13:33:49 -0300

    Criada a Function adicionar e pegarProduto EPreco

---
+++ js/app.js
@@ -0,0 +1,33 @@
+function adicionar(){
+    let carrinho = [];
+    const {produto, preco} = pegarProdutoEPreco();
+
+    if(produto && preco){
+        //Adicionar o produto ao carrinho
+        carrinho.push({ produto , preco});
+        alert(`Produto "${produto}" de preço R$${preco} adicionado ao carrinho!`);
+        console.log("Carrinho Atual:", carrinho);
+    } else {
+        alert("Por favor, selecione um produto válido.");
+    }
+}
+alert('Vamos la');
+pegarProdutoEPreco();
+
+function pegarProdutoEPreco(){       
+    //Obter o elemento do select
+    let listBox = document.getElementById('produto');   
+    let itemSelecionado = listBox.options[listBox.selectedIndex].value   
+
+    //Verificar se algum item foi selecionado
+    if (itemSelecionado){
+        //separa o nome do valor
+        let [pegarProduto, pegarPreco] = itemSelecionado.split(' - R$');
+        console.log("Produto:", pegarProduto);
+        console.log("Preço:", pegarPreco);
+
+        //retornar o produto e o preço
+        return{produto:pegarProduto,preco:parseFloat(pegarPreco)};
+    }
+
+}
